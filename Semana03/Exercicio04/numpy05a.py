c4921ccf0c7da285f4bdb236250e6ea8966fb4cd
lucasbonnas <157180692+lucasbonnas@users.noreply.github.com>
2024-01-26 19:45:15 -0300

    add files

---
+++ Semana03/Exercicio04/numpy05a.py
@@ -0,0 +1,11 @@
+import numpy as np
+
+l1 = [1,2,3]
+l2 = [4,5,6]
+a1 = np.array(l1)
+a2 = np.array(l2)
+
+# dot product
+dot = np.dot(a1,a2)
+print(dot)
+
---
+++ Semana03/Exercicio04/numpy07b.py
@@ -0,0 +1,6 @@
+import numpy as np
+
+a = np.array([[1,2],[3,4]])
+print(a)
+print(np.linalg.inv(a))
+print(np.linalg.det(a))
